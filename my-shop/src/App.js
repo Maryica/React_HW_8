899d683f120bb1aa47422f0994b24f1cfb18aa45
Maria <pavla0806@gmail.com>
2024-11-22 05:08:42 +0300

    HW8

---
+++ my-shop/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CatalogPage from "./pages/CatalogPage";
+import CartPage from "./pages/CartPage";
+import ProductPage from "./pages/ProductPage";
+
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} exact />
+                <Route path="/catalog" element={<CatalogPage />} />
+                <Route path="/cart" element={<CartPage />} />
+                <Route path="/product" element={<ProductPage />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
